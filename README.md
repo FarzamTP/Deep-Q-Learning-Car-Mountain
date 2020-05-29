fb155aec4ed2715bdee9e9b150a4bb974299ab72
FarzamTP <farzam.taghipour@gmail.com>
2020-05-30 02:12:43 +0430

    First phase of the CarMountain-v0 is now up!

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Q-Learning
 
+### Phase one
+This is the first phase of the project that focuses on training the car to reach the peak by initializing and updating the Q-Table.
+Here we use the [Bellman equation](https://en.wikipedia.org/wiki/Bellman_equation) as a simple [value iteration update](https://en.wikipedia.org/wiki/Markov_decision_process#Value_iteration).
+
+![Bellman equation](./images/Bellman-Equation.svg)
+
 ## Todo:
 
- - [ ] Initialize an environment
- - [ ] Edit Readme.md file
+ - [x] Initialize an environment
+ - [x] Simple model created.
+ - [x] Edit Readme.md file
--- main.py
+++ main.py
@@ -1,38 +1,61 @@
+import time
 import gym
 import numpy as np
 
 env = gym.make("MountainCar-v0")
-env.reset()
+
+lr = 0.2
+DISCOUNT = 0.95
+EPISODES = 10000
+
+SHOW_EVERY = 200
 
 
-def initialize_q_table():
-    print(env.observation_space.high)
-    print(env.observation_space.low)
-    print(env.action_space.n)
-
-    number_of_actions = env.action_space.n
-
-    COL_SIZE = 20
-    DISCRETE_OS_SIZE = [COL_SIZE] * len(env.observation_space.high)
-    print(DISCRETE_OS_SIZE)
-    DISCRETE_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
-    print(DISCRETE_os_win_size)
-    q_table = np.random.uniform(low=-2, high=0, size=(COL_SIZE, COL_SIZE, number_of_actions))
-    print(q_table.shape)
-    print(q_table)
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / DISCRETE_os_win_size
+    return tuple(discrete_state.astype(np.int))
 
 
 if __name__ == '__main__':
-    initialize_q_table()
+    """
+    Initializing the Q_Table, A 20 by 20 matrix with depth of env.action_space.n (In this case 3)
+    0 for going backward, 1 for staying and 2 for going forward
+    """
+    DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+    DISCRETE_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+    q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
-    done = False
+    """
+    Iterating over EPISODES to optimize the Q_table by using 'Bellman equation'
+    as a simple value iteration update.
+    """
+    for episode in range(EPISODES):
+        if episode % SHOW_EVERY == 0:
+            print("Episode:", episode)
+            render = True
+        else:
+            render = False
 
-    while not done:
-        action = 2
-        new_state, reward, done, _ = env.step(action)
-        print("new_state:", new_state)
-        print("reward:", reward)
-        print("done:", done)
-        env.render()
+        discrete_state = get_discrete_state(env.reset())
+
+        done = False
+
+        while not done:
+            action = np.argmax(q_table[discrete_state])
+            new_state, reward, done, _ = env.step(action)
+            new_discrete_state = get_discrete_state(new_state)
+            if render:
+                env.render()
+                time.sleep(0.02)
+
+            if not done:
+                max_future_q = np.max(q_table[new_discrete_state])
+                current_q = q_table[discrete_state + (action,)]
+                new_q = (1 - lr) * current_q + lr * (reward + DISCOUNT * max_future_q)
+                q_table[discrete_state + (action,)] = new_q
+            elif new_state[0] >= env.goal_position:
+                q_table[discrete_state + (action,)] = 0
+
+            discrete_state = new_discrete_state
 
     env.close()
