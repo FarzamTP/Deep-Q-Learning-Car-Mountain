27a5db931cd9883d20eddaf20ea1b792f0bb1552
FarzamTP <farzam.taghipour@gmail.com>
2020-05-30 03:20:16 +0430

    Model now uses epsilon decay.

--- README.md
+++ README.md
@@ -6,8 +6,39 @@ Here we use the [Bellman equation](https://en.wikipedia.org/wiki/Bellman_equatio
 
 ![Bellman equation](./images/Bellman-Equation.svg)
 
-## Todo:
+#### Result (Phase one)
+##### Not Using Epsilon Decay
+![phase one gif](./gifs/phase_one.gif)
 
- - [x] Initialize an environment
- - [x] Simple model created.
- - [x] Edit Readme.md file
+As it's obvious, the car at episode 1, has no idea what to do. But after only 500 episodes 
+it understands that by making progress to the right, he'll gain a point!
+But there is an interesting point there:
+***Although the car receives its reward by reaching the peak, as it's shown in the gif, it tried to minimize the spent time. To do that, at first it decreases amount of the path in goes forward and uses its gained velocity to reach the top!***
+
+##### Using Epsilon Decay
+![phase one gif](./gifs/phase_one_epsilon_decay.gif)
+
+Although the car reaches the peak in a quite acceptable time, by using epsilon decay we make model to ***explore*** more in order to find a better approach!
+And as it's shown in above gif, the car minimizes it's spent time to reach the peak.
+
+### How to use:
+First clone the repository:
+```shell script
+$ git clone https://github.com/FarzamTP/Deep-Q-Learning-Car-Mountain.git
+$ cd Deep-Q-Learning-Car-Mountain
+```
+To setup the `virtual environment` and `activating` it:
+```shell script
+$ python3 -m venv venv
+$ source venv/bin/activate
+```
+
+And to install the requirements:
+```shell script
+(venv)$ pip3 install -r requirements.txt
+```
+
+and run the `main.py` script:
+```shell script
+(venv)$ python3 main.py
+```
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-import time
 import gym
+from matplotlib import animation
+import matplotlib.pyplot as plt
 import numpy as np
+import time
+
+
+def save_frames_as_gif(frames, path='./gifs/', filename='gym_animation.gif'):
+    plt.figure(figsize=(frames[0].shape[1] / 100.0, frames[0].shape[0] / 100.0), dpi=50)
+
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=50)
+    anim.save(path + filename, writer='imagemagick', fps=60)
+    print("File saved!")
+    return
+
 
 env = gym.make("MountainCar-v0")
 
 lr = 0.2
 DISCOUNT = 0.95
-EPISODES = 10000
+EPISODES = 5000
 
-SHOW_EVERY = 200
+SHOW_EVERY = 2000
+
+epsilon = 0.5
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES // 2
+
+epsilon_decaying_value = epsilon // (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 
 def get_discrete_state(state):
@@ -24,6 +48,12 @@ if __name__ == '__main__':
     DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
     DISCRETE_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
     q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+
+    """
+    Frames list captures the frames of the successful car episode,
+    and the save_frames_as_gif() will convert it to gif.
+    """
+    frames = []
 
     """
     Iterating over EPISODES to optimize the Q_table by using 'Bellman equation'
@@ -45,7 +75,10 @@ if __name__ == '__main__':
             new_state, reward, done, _ = env.step(action)
             new_discrete_state = get_discrete_state(new_state)
             if render:
+                frames.append(env.render(mode="rgb_array"))
                 env.render()
+
+                """Feel free to change the sleep time if the car is too slow or too fast for you."""
                 time.sleep(0.02)
 
             if not done:
@@ -54,8 +87,13 @@ if __name__ == '__main__':
                 new_q = (1 - lr) * current_q + lr * (reward + DISCOUNT * max_future_q)
                 q_table[discrete_state + (action,)] = new_q
             elif new_state[0] >= env.goal_position:
+                print(f"We made it on episode {episode}")
                 q_table[discrete_state + (action,)] = 0
 
             discrete_state = new_discrete_state
 
+        if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
+            epsilon -= epsilon_decaying_value
+
+    save_frames_as_gif(frames, filename="phase_one_epsilon_decay.gif")
     env.close()
